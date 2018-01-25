ac6ee2000cbffaf86cc9b7d1fb23b02208ce13e6
Lameck <lameck2@hotmail.com>
2018-01-25 11:14:18 -0300

    Adicionando modulos node js

--- fileserver.js
+++ fileserver.js
@@ -2,6 +2,17 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.get('/',function(req, res) {
+	fs.readFile('mectal.html', function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
+});
+
 
 /* Criando server http */
 http.createServer(function (req, res) {
@@ -9,11 +20,6 @@ http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
 	var filename = "." + q.pathname;
 	fs.readFile(filename, function(err, data) {
-		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404 Not Found");
-		}
-
 		res.writeHead(200, {'Content-Type' : 'text/html'});
 		res.write(data);
 		return res.end();
