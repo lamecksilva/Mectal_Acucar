290590c705e259965c48d813e6283623fa521c40
Lameck <lameck2@hotmail.com>
2018-01-25 10:45:14 -0300

    Adicionando comentarios no servidor

--- fileserver.js
+++ fileserver.js
@@ -1,8 +1,11 @@
+/* importando modulos em variaveis */
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+/* Criando server http */
 http.createServer(function (req, res) {
+
 	var q = url.parse(req.url, true);
 	var filename = "." + q.pathname;
 	fs.readFile(filename, function(err, data) {
