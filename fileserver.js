fd9b858c62fa0114e5cd048cb19ccf663d2dfdfc
Lameck <lameck2@hotmail.com>
2018-01-27 18:10:18 -0300

    Upload files

--- fileserver.js
+++ fileserver.js
@@ -1,18 +1,4 @@
 var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var app = require('./app');
 
-http.createServer(function (req, res) {
-	var q = url.parse(req.url, true);
-	var filename = "." + q.pathname;
-	fs.readFile(filename, function(err, data) {
-		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404 Not Found");
-		}
-
-		res.writeHead(200, {'Content-Type' : 'text/html'});
-		res.write(data);
-		return res.end();
-	});
-}).listen(8080);
+http.createServer(app.handleRequest).listen(8080);
